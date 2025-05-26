414642229234e08c6732b51014bb20aff9c672ff
Tab-Febrian <febianfebian323@gmail.com>
2025-05-26 09:13:40 +0700

    feat/ApiSetup

---
+++ app/src/main/java/com/example/pas_2_22/APIService.java
@@ -0,0 +1,12 @@
+package com.example.pas_2_22;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIService {
+    @GET("search_all_teams.php?s=Soccer&c=Spain")
+    Call<TeamResponse> getSpanishLeagueTeams();
+
+    @GET("lookup_all_teams.php?id=4328")
+    Call<TeamResponse> getAllTeams();
+}
